d8bd05d2bf67165e6fc17b620e805961f9a4028f
Colin Rupp <colin@thinkful.com>
2018-09-04 12:11:59 -0700

    added id finder to api

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get('/api/notes', (req, res) => {
   res.json(data);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+  res.json(data.find(item => item.id === Number(id)));
+});
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
