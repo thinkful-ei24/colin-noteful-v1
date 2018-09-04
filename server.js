515a5069e671746ad6ecc1347f12d55bed60d9ec
Colin Rupp <colin@thinkful.com>
2018-09-04 11:39:45 -0700

    added npm start and initialized server.js

--- server.js
+++ server.js
@@ -3,6 +3,17 @@
 // Load array of notes
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+
 
 // INSERT EXPRESS APP CODE HERE...
