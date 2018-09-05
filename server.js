47de029edfd9abf313701c4c8432ebd069e34959
Colin Rupp <colin@thinkful.com>
2018-09-05 13:32:35 -0700

    added simDB to app

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 // Load array of notes
 const data = require('./db/notes');
+const simDB = require('./db/simDB');
+const notes = simDB.initialize(data);
+
 //Load modules
 const { PORT } = require('./config')
 const express = require('express');
