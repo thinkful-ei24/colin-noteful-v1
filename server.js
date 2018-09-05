ff1ab36e4f6ac4c16e00f233256702ca4446cbbb
Colin Rupp <colin@thinkful.com>
2018-09-05 11:19:27 -0700

    added config.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 // Load array of notes
 const data = require('./db/notes');
 
+const { PORT } = require('./config')
+
 const express = require('express');
 
 const app = express();
@@ -22,7 +24,7 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(data.find(item => item.id === Number(id)));
 });
 
-app.listen(8080, function () {
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
